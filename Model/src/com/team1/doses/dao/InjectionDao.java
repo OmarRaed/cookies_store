5747748b155b21f8c312788c8524dfc15a6bb147
Abdelrahman Tareq <abdotarekfathy@gmail.com>
2019-12-25 10:20:03 +0200

    get data from db has problem

---
+++ Model/src/com/team1/doses/dao/InjectionDao.java
@@ -0,0 +1,11 @@
+package com.team1.doses.dao;
+
+
+import com.team1.doses.dto.InjectionDto;
+
+import java.util.ArrayList;
+
+public interface InjectionDao {
+    public ArrayList<InjectionDto> getAllInjections();
+
+}
